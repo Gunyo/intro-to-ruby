19576f2df5539c41a06ffeea875f6c50cc198a50
Yuusuke Nagata <yusukenagata@hotmail.com>
2021-03-15 11:30:35 +1000

    terminal input

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
-time = 10
-if time > 10
+time = 11
+good_student = true
+if time > 10 and !good_student
     puts "You're late!"
 elsif time == 10
     puts "You're right on time!"
@@ -7,3 +8,11 @@ else
     puts "Good morning!"
 end
     
+
+number = 4
+puts "Greater than 3" if number > 3
+
+print "Enter your name: "
+name = gets
+# puts "Hello, #{name}!"
+p name
